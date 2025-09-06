f0722806133b8ea9a7a2f1b67c3673a0761551f0
asarama <arshameslami@hotmail.com>
2025-09-05 17:23:01 -0700

    Playing around with return types and how expect works.
    drop target dir

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    let bytes_read = io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You wrote {bytes_read} bytes");
+    println!("You guessed: {guess}");
 }
